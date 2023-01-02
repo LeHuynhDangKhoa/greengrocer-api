2604874772750e6120ad05fc46ced81e159e2cf8
LeHuynhDangKhoa <lehuynhdangkhoa1985@gmail.com>
2023-01-02 19:21:31 +0700

    update

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    # SECRET_KEY = '57e19ea558d4967a552d03deece34a70'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_AVATAR = "images/avatar/" 
     UPLOAD_PRODUCT = "images/product/" 
